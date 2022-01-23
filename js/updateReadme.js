b10399826c3432e49049a5a700bdd3828b8a2a08
Patrick Hettich <patrick_hettich@hotmail.com>
2022-01-23 19:12:46 +0100

    adding the actual table

--- js/updateReadme.js
+++ js/updateReadme.js
@@ -3,12 +3,12 @@ const fs = require("fs");
 module.exports.updateReadme = (apps) => {
     let readme = fs.readFileSync('templateReadme.md', { encoding: 'utf8', flag: 'r' });
 
-    appsTableItems = '|App|Installs|Released|\n|:---|:---|:---|\n'
+    appsTable = '|App|Installs|Released|\n|:---|:---|:---|\n'
 
     apps.forEach(app => {
-        appsTableItems += `[<img src="${app.icon}" width="50" height="50" style="border-radius:10%;box-shadow:2px 2px 5px grey;">](${app.url})</br>${app.appName}|${app.maxInstalls}|${app.released}\n`
+        appsTable += `[<img src="${app.icon}" width="50" height="50" style="border-radius:10%;box-shadow:2px 2px 5px grey;">](${app.url})</br>${app.appName}|${app.maxInstalls}|${app.released}\n`
     });
-    readme = readme.replace('[APPS]', apps[0].appName);
+    readme = readme.replace('[APPS]', appsTable);
 
     fs.writeFileSync('README.md', readme);
 }
